8f2044c06ec4dd45d02824a4c65a2c767cc10ffa
Richard Hansen <rhansen@rhansen.org>
2020-10-28 15:21:44 -0400

    Use arrow function in `client.js`
    Etherpad now supports modern JavaScript and requires modern browsers.

--- static/js/client.js
+++ static/js/client.js
@@ -2,9 +2,7 @@
 
 /* global $, clientVars, exports */
 
-/* etherpad-lite uses uglify 2.x, so stick to ES5 */
-
-exports.postToolbarInit = function () {
+exports.postToolbarInit = () => {
   if (!clientVars.ep_openid_connect.permit_displayname_change) {
     $('#myusernameedit').attr('disabled', true);
   }
