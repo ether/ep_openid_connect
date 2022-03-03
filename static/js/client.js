9fb730670c6ebac3acf37d3e503d5f125642335d
Richard Hansen <rhansen@rhansen.org>
2022-03-02 21:08:19 -0500

    Move displayname handling to ep_user_displayname

--- static/js/client.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-exports.postToolbarInit = () => {
-  const {ep_openid_connect: {permit_displayname_change = true} = {}} = clientVars;
-  if (!permit_displayname_change) $('#myusernameedit').attr('disabled', true);
-};
